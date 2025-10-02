f3744f90d8a084b56096a318f56a35e4fdb17cb9
fpaglia <fpaglia@student.42vienna.com>
2025-10-02 13:07:40 +0200

    first pass on #32 included a demo file to observe the behavior

---
+++ source/strings/str_isquoted.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   str_isquoted.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpaglia <fpaglia@student.42vienna.com      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 09:12:23 by fpaglia           #+#    #+#             */
+/*   Updated: 2025/10/01 12:54:39 by fpaglia          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+int	str_isquoted(char c)
+{
+	static int quote = 0;
+	if (c == '"' || c == '\'')
+	{
+		if (quote == 0)
+			quote = (int)c;
+		else if (quote == (int)c)
+		{
+			quote = 0;
+			return ((int)c);
+		}
+			
+	}
+	return (quote);
+}
