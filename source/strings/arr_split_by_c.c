930e704ea6f33fa4ccbbe357d0af0eaa44f560be
fpaglia <fpaglia@student.42vienna.com>
2025-10-03 12:41:58 +0200

    finalized array split by char with demo program

--- source/strings/arr_split_by_c.c
+++ source/strings/arr_split_by_c.c
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 16:43:06 by fpaglia           #+#    #+#             */
-/*   Updated: 2025/10/03 12:21:21 by fpaglia          ###   ########.fr       */
+/*   Updated: 2025/10/03 12:40:14 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,8 @@ char	*ft_strncpy(char *src, size_t len)
 	str[i] = '\0';
 	return (str);
 }
-static char **set_array(char *str, char c, bool eval_quote)
+
+static char	**set_array(char *str, char c, bool eval_quote)
 {
 	int		i;
 	int		items;
@@ -50,7 +51,7 @@ static char **set_array(char *str, char c, bool eval_quote)
 	while (str[i])
 	{
 		if (evaluate_quote(str[i], eval_quote) == 0
-			&& str[i] != c && (str[i+1] == c || str[i+1] == 0))
+			&& str[i] != c && (str[i + 1] == c || str[i + 1] == 0))
 			items++;
 		i++;
 	}
@@ -59,38 +60,41 @@ static char **set_array(char *str, char c, bool eval_quote)
 		return (NULL);
 	return (arr);
 }
-/*
-static char *find_end(char *str, char c, bool eval_quote)
+
+static char	*find_end(char *str, char c, bool eval_quote)
 {
-	
+	int		quote;
+	char	*end;
+
+	end = str + 1;
+	quote = 0;
+	while (1)
+	{
+		quote = evaluate_quote(*end, eval_quote);
+		if (quote == 0 && (*end == c || *end == 0))
+			break ;
+		end++;
+	}
+	return (end);
 }
-*/
+
 char	**arr_split_by_c(char *str, char c, bool eval_quote)
 {
 	char	**arr;
 	char	*end;
 	int		i;
-	int		quote;
 
 	arr = set_array(str, c, eval_quote);
 	if (arr == NULL)
 		return (NULL);
 	i = 0;
-	quote = 0;
 	while (1)
 	{
 		while (evaluate_quote(*str, eval_quote) == 0 && *str == c)
 			str++;
 		if (*str == '\0')
-			break;
-		end = str + 1;
-		while (1)
-		{
-			quote = evaluate_quote(*end, eval_quote);
-			if (quote == 0 && (*end == c || *end == 0))
-				break;
-			end++;
-		}
+			break ;
+		end = find_end(str, c, eval_quote);
 		arr[i] = ft_strncpy(str, (size_t)(end - str));
 		if (arr[i] == NULL)
 			return (arr_free(arr), NULL);
