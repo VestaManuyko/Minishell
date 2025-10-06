a513d55aad18dc3549db9fdd3a3925c95deea482
Paglia Francesco <125829321+francescoPagliaPersonal@users.noreply.github.com>
2025-10-06 13:10:45 +0200

    Create main.c
    very first approach to the main.c of the minishell.

---
+++ source/main.c
@@ -0,0 +1,21 @@
+
+#include <minishell.h>
+
+int main(int ac, char **av, char **env)
+{
+  t_shell  shell;
+
+  init_shell(&shell);
+  while (1)
+    {
+      if (get_command(&shell))
+      {
+        populate_programs(&shell);
+        if (validate_programs(&shell))
+        {
+          run_programs(&shell);
+          free_programs(&shell)
+        }
+      }
+    }
+}
