5eb403106730f79d61c43bb5bb4d9949edd5f16f
Vesta <vesta.manuyko@gmail.com>
2025-10-06 14:11:34 +0300

    create file structure

--- source/input/prompt.c
+++ source/input/prompt.c
@@ -1,84 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vmanuyko <vmanuyko@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 11:29:16 by vmanuyko          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2025/10/03 12:51:42 by vmanuyko         ###   ########.fr       */
-=======
-/*   Updated: 2025/10/03 12:36:13 by vmanuyko         ###   ########.fr       */
->>>>>>> d2f876d (Prompt display by executing the program)
+/*   Updated: 2025/10/06 13:18:31 by vmanuyko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "minishell.h"
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
-//delete later upon adding libft.h
-size_t	ft_strlen(const char *str)
-{
-	size_t	c;
+#include <minishell.h>
 
-	c = 0;
-	while (str[c])
-		c++;
-	return (c);
-}
-
-char	*ft_strdup(const char *s)
-{
-	size_t		len;
-	size_t		i;
-	char		*dup;
-
-	len = 0;
-	while (s[len])
-		len++;
-	dup = malloc(len + 1);
-	if (!dup)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		dup[i] = s[i];
-		i++;
-	}
-	dup[i] = 0;
-	return (dup);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	sum_len;
-	char	*str;
-	size_t	i;
-	size_t	j;
-
-	sum_len = ft_strlen(s1) + ft_strlen(s2);
-	str = malloc(sum_len + 1);
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		str[i] = s2[j];
-		j++;
-		i++;
-	}
-	str[i] = 0;
-	return (str);
-}
 // char *get_prompt gets the USER env variable and appends it with 
 // the shell_prompt, so that readline may display prompt as example:
 //USER/minishell>
@@ -121,7 +54,6 @@ int main(int argc, char **argv, char **env)
 	while (1)
 	{
 		line = readline(prompt);
-<<<<<<< HEAD
 		if (!line)
 		{
 			perror("Error");
@@ -132,7 +64,4 @@ int main(int argc, char **argv, char **env)
 		free(line);
 	}
 	free(prompt);
-=======
-	}
->>>>>>> d2f876d (Prompt display by executing the program)
 }
