28a184d9abb67419c79e4f15689d03f07eaaa9f4
fpaglia <fpaglia@student.42vienna.com>
2025-10-08 15:47:25 +0200

    modified tar init to accomodate the creation of an empty array that comes handy everytime we want to modify an array of unknown expansion

--- source/strings/tar_init.c
+++ source/strings/tar_init.c
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 09:57:29 by fpaglia           #+#    #+#             */
-/*   Updated: 2025/10/07 12:07:10 by fpaglia          ###   ########.fr       */
+/*   Updated: 2025/10/08 15:36:53 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,17 +29,26 @@ t_arr	*tar_init(char **src)
 	t_arr	*tarr;
 	int		i;
 
-	if (src == NULL)
-		return (NULL);
 	tarr = (t_arr *)malloc(sizeof(t_arr));
 	if (tarr == NULL)
 		return (NULL);
-	tarr->size = arr_size(src) -1;
-	tarr->capacity = tarr->size + 1;
+	if (src != NULL)
+	{
+		tarr->size = arr_size(src) -1;
+		tarr->capacity = tarr->size + 1;
+	}
+	else 
+	{
+		tarr->size = 0;
+		tarr->capacity = 8;
+	}
 	tarr->arr = (char **)malloc(tarr->capacity * sizeof(char *));
 	if (tarr->arr == NULL)
 		return (free(tarr), NULL);
-	if (arr_deepcpy(src, &tarr->arr, tarr->capacity) == 0)
-		return (NULL);
+	if (src != NULL)
+	{
+		if (arr_deepcpy(src, &tarr->arr, tarr->capacity) == 0)
+			return (NULL);
+	}
 	return (tarr);
 }
