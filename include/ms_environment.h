7def0b031a7fc490459964f81a13a106cbd076f4
fpaglia <fpaglia@student.42vienna.com>
2025-10-14 16:05:48 +0200

    added documentation to env funct

--- include/ms_environment.h
+++ include/ms_environment.h
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 16:24:15 by fpaglia           #+#    #+#             */
-/*   Updated: 2025/10/13 18:50:40 by fpaglia          ###   ########.fr       */
+/*   Updated: 2025/10/14 16:05:28 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 # include "ms_structs.h"
 
-/* Given the environment array search for the id of the string that
+/* 
+ * Given the environment array search for the id of the string that
  * begins with *key.
  *
  * RETURNS:
@@ -24,7 +25,8 @@
  */
 int		env_getid(char **arr, char *key);
 
-/* Given an id found with env_getid, stores at the address of **str
+/* 
+ * Given an id found with env_getid, stores at the address of **str
  * a copy of the value found after the '=' sign.
  *
  * RETURNS:
@@ -34,7 +36,8 @@ int		env_getid(char **arr, char *key);
  */
 int		env_getvalue(char **arr, char **str, int id);
 
-/* Modifies the env array based on the parameter string.
+/* 
+ * Modifies the env array based on the provided string.
  * if the key is not found the string is appended in its entirety
  * else it's substituted with the new string.
  *
@@ -45,8 +48,31 @@ int		env_getvalue(char **arr, char **str, int id);
  */
 int		env_entry_update(t_arr *env, char *str);
 
+/* 
+ * Search through the env for the provided key, if found will remove the
+ * string from the env array.
+ * The key is expanded via str_clearquotes before is analized.
+ *
+ * RETURNS:
+ * - 1 on success
+ * - 0 on missing key
+ */
 int		env_entry_remove(t_arr *env, char *key);
 
+/* 
+ * Given a string finds the portion of it that preceeds the equal sign.
+ * The key is expanded via str_clearquotes before is analized.
+ * The string must be compliant with a valid key structure:      
+ * starts with '_' or a ft_isalpha() char and includes only  ft_isalphanum()
+ * and '_'.
+ * If the original string is empty or the '=' char is the first one, 
+ * an error is returned and Errno is set to: EINVAL
+ * It is the responsibility of the caller to free the original string.
+ *
+ * RETURNS:
+ * - a pointer to the newly allocated string;
+ * - NULL in case the the key is wrongly doesn't respect the convention. 
+ */
 char	*env_getkey(char *str);
 
 #endif
