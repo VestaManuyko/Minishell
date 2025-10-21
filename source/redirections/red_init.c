72bdf3f39c49cac833ae3352bd13c6310ddd0a82
fpaglia <fpaglia@student.42vienna.com>
2025-10-21 17:20:54 +0200

    norm compliant redirection functs

--- source/redirections/red_init.c
+++ source/redirections/red_init.c
@@ -6,25 +6,20 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 23:24:40 by fpaglia           #+#    #+#             */
-/*   Updated: 2025/10/17 23:24:59 by fpaglia          ###   ########.fr       */
+/*   Updated: 2025/10/21 16:27:52 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include "ms_strings.h"
-#include "ms_structs.h"
 #include <minishell.h>
-#include <stdio.h>
-#include <stdlib.h>
 
-t_red	*red_init(t_redtype type, char *str)
+t_red	*red_init(t_redtype type, char *raw)
 {
-	t_red *item;
+	t_red	*item;
 
 	item = (t_red *)calloc(1, sizeof(t_red));
 	if (item == NULL)
 		return (NULL);
 	item->type = type;
-	item->val = str;
+	item->raw = raw;
 	return (item);
 }
