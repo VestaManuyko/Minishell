639caf1d47cc288f11935f8c7a501e64515250d2
fpaglia <fpaglia@student.42vienna.com>
2025-10-23 10:31:03 +0200

    included documentation at cmd_validate_pipes

--- include/ms_commands.h
+++ include/ms_commands.h
@@ -6,7 +6,7 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 15:41:04 by fpaglia           #+#    #+#             */
-/*   Updated: 2025/10/23 10:15:30 by fpaglia          ###   ########.fr       */
+/*   Updated: 2025/10/23 10:30:07 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,13 @@
 
 # include <libft.h>
 
-/* 
+/*
+ * Given a string evaluates if there are some chars between the portion of it 
+ * limited by "|,\n,\0"   
+ *
+ * RETURNS:
+ * - 1 if the portion of string is not empty.
+ * - 0 otherwise.
  */
 int	cmd_validate_pipes(char *str);
 
