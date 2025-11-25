df17e4ddbce757600b98d281b68c7824616d1f5b
fpaglia <fpaglia@student.42vienna.com>
2025-11-25 12:43:05 +0100

    in case the prog->size is 0 the function is skipped

--- source/commands/cmd_parse_progs.c
+++ source/commands/cmd_parse_progs.c
@@ -6,16 +6,19 @@
 /*   By: fpaglia <fpaglia@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 11:06:57 by fpaglia           #+#    #+#             */
-/*   Updated: 2025/11/24 12:39:40 by fpaglia          ###   ########.fr       */
+/*   Updated: 2025/11/25 12:42:20 by fpaglia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ms_strings.h"
 #include <minishell.h>
 
 int	cmd_parse_progs(t_prog *proc, t_arr *env)
 {
 	t_arr	*tar;
 
+	if (proc->prog->size == 0)
+		return (1);
 	if (ft_strncmp((char *)proc->prog->arr[0], "export", 6) == 0)
 		tar = cmd_expand_export(proc->prog, env);
 	else
