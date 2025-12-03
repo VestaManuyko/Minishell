0eba9ca83835e3e995ea20c5e5fb7ebbc34e836e
Vesta Manuyko <Vesta.manuyko@gmail.com>
2025-12-03 15:17:18 +0100

    fix: #166

--- source/execution/exec_prog.c
+++ source/execution/exec_prog.c
@@ -63,14 +63,14 @@ void	set_status(int status, t_shell *sh)
 */
 int	dup_fds(t_prog *item)
 {
-	if (item->fd_io[0] != 0)
+	if (item->fd_io[0] > 0)
 	{
 		if ((dup2(item->fd_io[0], STDIN_FILENO)) == -1)
-			return (0);
+		return (0);
 		close (item->fd_io[0]);
 		item->fd_io[0] = -1;
 	}
-	if (item->fd_io[1] != 1)
+	if (item->fd_io[1] > 1)
 	{
 		if ((dup2(item->fd_io[1], STDOUT_FILENO)) == -1)
 			return (0);
