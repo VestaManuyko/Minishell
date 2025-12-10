8a2e591065658456eb246765fe01c380cc3dbe75
francescoPagliaPersonal <fra.py.study@gmail.com>
2025-12-10 06:00:06 +0100

    this commit Fixes #173 making the general case of a $sign being the last item of a portion of unquoted string which is followed by a portion that includes a quote hence the string has not reached its end.

--- source/strings/istr_expand_var.c
+++ source/strings/istr_expand_var.c
@@ -33,19 +33,13 @@ static int	check_connectivity(char *str)
 	return (conn);
 }
 
-/* to try:
- * find last $ sign, check if the next is a null then remove it 
- * from the string 
-*/
 char	*set_value(t_istr *tmp, int item, int max, t_shell *sh)
 {
 	char	*line;
 
-
-	if (tmp->type == 0 && item < max && tmp->str[0] == '$' && tmp->len == 1)
-		line = ft_strdup("");
-	else
-		line = str_expand(dollar, tmp->str, 0, sh);
+	if (tmp->str[tmp->len - 1] == '$' && item < max && tmp->type == 0)
+		tmp->str[tmp->len - 1] = '\0';
+	line = str_expand(dollar, tmp->str, 0, sh);
 	return (line);
 }
 
